8f3baa7a1db579d6fa442842a822ca2cbbc6884d
bikramia <bikram.rhce@gmail.com>
2024-07-23 17:52:56 -0400

    first commit

---
+++ 1.Events-Handler.tsx
@@ -0,0 +1,20 @@
+
+
+function ListGroup() {
+    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]
+
+    // Event handler
+    // const handleClick = (event: MouseEvent)=> console.log(event);
+
+    return (
+        <>
+        <h1>List</h1>
+        {items.length === 0 && <p>No item found</p>}
+        <ul className="list-group">
+            {items.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+        </>
+    )
+}
