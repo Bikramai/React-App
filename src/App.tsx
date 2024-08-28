46f9010fd9fa68e9b85bb037783df11108b12f1c
bikramia <bikram.rhce@gmail.com>
2024-08-28 08:00:14 -0400

    Adding Icons -App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Button from "./components/Button";
+import { BsCalendarCheckFill } from "react-icons/bs";
 
 
 function App() { 
   return (
     <div>
       <Button onClick={() => console.log('Clicked')}>My Dyanamic Button</Button>
+      <BsCalendarCheckFill color="red" size="300"/>
     </div>
   );
 }
