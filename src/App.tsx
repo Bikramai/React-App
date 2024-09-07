2c449e09a86a4a86bd86778117bbc9246422bf08
bikramia <bikram.rhce@gmail.com>
2024-09-07 18:50:47 -0400

    Install immer and simplifying  Update Logic with immer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 // import Message from "./Message";
 
 import { useState } from "react";
+import produce from 'immer';
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -13,11 +14,18 @@ function App() {
 
   const handleClick = () => {
     // Add
-    setBugs(bugs.map((bug) => (bug.id ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id ? { ...bug, fixed: true } : bug)));
+
+    // using immer
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1);
+      if (bug) bug.fixed = true;
+    }))
   };
 
   return (
     <div>
+      {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
