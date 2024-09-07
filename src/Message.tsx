809417d33c4c74bdc09afc02b1117e44db49758e
bikramia <bikram.rhce@gmail.com>
2024-09-07 14:32:43 -0400

    Understanding Strict Mode

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,8 @@
 // impure components
+let count = 0;
 
 const Message = () => {
-    let count = 0;
+    console.log('Message called', count);
     count++;
     return <div>Message { count }</div>;
 };
