7244029148a67d1aecbdc113dfc5c33e87ce1e77
bikramia <bikram.rhce@gmail.com>
2024-09-07 15:00:21 -0400

    Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 // import Button from "./components/Button";
 // import { BsCalendarCheckFill } from "react-icons/bs";
 // import Like from "./components/Like";
+// import Message from "./Message";
 
-import Message from "./Message";
+import { useState } from "react";
 
 
 function App() { 
+  const [drink, setDrink] = useState({
+    title: "Lemanade", 
+    price: 5,
+  });
+
+  const handleClick = () => {  //updating Objects
+    setDrink({ ...drink, price: 6 });
+  }; 
+
   return (
     <div>
-      <Message />
-      <Message />
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
